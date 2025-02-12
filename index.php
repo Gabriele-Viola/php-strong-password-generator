2ec02e42c989064c7466b2a1304d32e9a8c76736
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-02-12 17:49:25 +0100

    create password only number

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+$lenght = isset($_GET['long']) ? $_GET['long'] : 0;
+
+function generate(int $lenghtPass)
+{
+
+    do {
+        static $counter = 0;
+        $generated[$counter - 1] = rand(0, 9);
+        $counter++;
+        $password = implode("", $generated);
+    } while ($counter <= $lenghtPass);
+    return $password;
+}
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
+</head>
+
+<body>
+
+    <h1>password generator</h1>
+    <form action="">
+        <label for="long">Lunghezza della password</label>
+        <input type="number" id="long" name="long">
+        <button type="submit">invio</button>
+    </form>
+
+    <p>
+        <?php
+        echo $lenght;
+
+        ?>
+    </p>
+    <p>
+        <?php
+        // var_dump(generate($lenght));
+        echo generate($lenght);
+        ?>
+    </p>
+
+</body>
+
+</html>
